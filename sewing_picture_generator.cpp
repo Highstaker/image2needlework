ad65020ccc23b4e559b35c4a082b5a7fe0140d4c
Highstaker <heights999@yandex.ru>
2013-01-19 20:00:49 +0400

    added multithreading support (draft)

--- sewing_picture_generator.cpp
+++ sewing_picture_generator.cpp
@@ -95,8 +95,8 @@ int main(int argc, char **argv)
 
 	for(int i=0; i<thread_amount ; i++)
 	{
-		int thread_param = thread_amount *100 + i;
-		pthread_create(&thread_id[i],0,&renderOutput,(void*)thread_param);
+		//int thread_param = thread_amount *100 + i;
+		pthread_create(&thread_id[i],0,&renderOutput,(void*)i);
 	cerr << "Mile1" << endl;//debug	
 	}
 
@@ -181,10 +181,41 @@ void* renderOutput(void *lp)
 {
 cerr << "tMile1" << endl;//debug
 
+int param = (int)lp;
+cerr << "param: " << param << endl;//debug
+int startx = 0;
+int endx = out_width;
+int starty = 0;
+int endy =out_height;
 
 
-for(int i = 0 ; i< out_width ; i++)
-	for(int j = 0; j < out_height ; j++)
+if(thread_amount == 1)
+{
+startx = 0;
+endx = out_width;
+starty = 0;
+endy =out_height;
+}
+
+if(thread_amount == 2)
+{
+startx = 0 + param * out_width/2;
+endx = out_width/2 + param * out_width/2;
+starty = 0;
+endy = out_height;
+//if(param == 0){return 0;};//debug
+}
+
+if(thread_amount == 4)
+{
+startx = param/2 * out_width/2;
+endx = param/2 * out_width/2 + out_width/2;
+starty = param%2 * out_height/2;
+endy = param%2 *out_height/2 + out_height/2;
+}
+
+for(int i = startx ; i< endx ; i++)
+	for(int j = starty; j < endy ; j++)
 	{
 			//cerr << "tMile1.5" << endl;//debug
 		if(i%e<line_width)
@@ -217,36 +248,61 @@ cFreetype symbols( "/usr/share/fonts/truetype/freefont/FreeSans.ttf",cell_size) 
 
 	//cerr << "Mile2" << endl;//debug
 
-for(int k = 0; k< n_cells_x; k++)
-	for(int n = 0; n< n_cells_y; n++)
-	{
-		symbols.get_char_pixel_data(65 + (in_image -> getPalettePixelIndex(k, (in_image->m_height ) - 1 - n)));
-		c = in_image -> getPixel(k, (in_image->m_height ) - 1 - n);
+if(thread_amount == 1)
+{
+startx = 0;
+endx = n_cells_x;
+starty = 0;
+endy =n_cells_y;
+}
+
+if(thread_amount == 2)
+{
+startx = 0 + param * n_cells_x/2;
+endx = n_cells_x/2 + param * n_cells_x/2;
+starty = 0;
+endy = n_cells_y;
+//if(param == 0){return 0;};//debug
+}
+
+if(thread_amount == 4)
+{
+startx = param/2 * n_cells_x/2;
+endx = param/2 * n_cells_x/2 + n_cells_x/2;
+starty = param%2 * n_cells_y/2;
+endy = param%2 *n_cells_y/2 + n_cells_y/2;
+}
+
+	for(int k = startx; k< endx; k++)
+		for(int n = starty; n< endy; n++)
+		{
+			symbols.get_char_pixel_data(65 + (in_image -> getPalettePixelIndex(k, (in_image->m_height ) - 1 - n)));
+			c = in_image -> getPixel(k, (in_image->m_height ) - 1 - n);
 			//int lightness = 255;
-		int lightness = 0.3 * c.r + 0.59 * c.g + 0.11 * c.b;
+			int lightness = 0.3 * c.r + 0.59 * c.g + 0.11 * c.b;
 //int lightness = c.max_channel_value()/2 + c.min_channel_value()/2;
 //cerr << "c.r: " << c.r << "\t c.max_channel_value: " << c.max_channel_value() << "\t c.min_channel_value: " << c.min_channel_value() <<  "\t lightness: " << lightness << endl; //debug
-		if(lightness > 122)
-		{
-			c.r = 0; c.b = 0; c.g = 0;
-		}
-		else 
-		{
-			c.r = 255; c.g = 255; c.b = 255;
-		}
-
-		for(int i=0;i<symbols.rows;i++)
-			for(int j=0;j<symbols.width;j++)
+			if(lightness > 122)
 			{
-				unsigned char gray = symbols.buffer[i*symbols.width+j];
-				c.a = gray ;					
-				int x1 = line_width + e*k + j ; if(x1<0)x1=0; if(x1>=out_width)x1=(out_width-1);
-				int y1 = out_height - line_width - e * n - i ; if(y1<0)y1=0; if(y1>=out_height)x1=(out_height-1);
-				out_image -> blendPixel(c,x1, y1,BLEND_MODE_NORMAL);
-					//Colour_24 c_test = out_image -> getPixel(k,n); cerr << (int)c_test.a << endl;//debug
+				c.r = 0; c.b = 0; c.g = 0;
+			}
+			else 
+			{
+				c.r = 255; c.g = 255; c.b = 255;
 			}
 
+			for(int i=0;i<symbols.rows;i++)
+				for(int j=0;j<symbols.width;j++)
+				{
+					unsigned char gray = symbols.buffer[i*symbols.width+j];
+					c.a = gray ;					
+					int x1 = line_width + e*k + j ; if(x1<0)x1=0; if(x1>=out_width)x1=(out_width-1);
+					int y1 = out_height - line_width - e * n - i ; if(y1<0)y1=0; if(y1>=out_height)x1=(out_height-1);
+					out_image -> blendPixel(c,x1, y1,BLEND_MODE_NORMAL);
+					//Colour_24 c_test = out_image -> getPixel(k,n); cerr << (int)c_test.a << endl;//debug
+				}
 		}
+
 
 cerr << "thread lp: " << (int)lp << endl;//debug
 return 0;
